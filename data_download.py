f2963e9a58fb7033fc9422c9358791b0985fecac
ggenois <guillaumegenois@yahoo.ca>
2024-10-08 10:37:41 -0400

    cleanage

--- data_download.py
+++ data_download.py
@@ -8,20 +8,23 @@ import json
 headers = {'Accept': 'application/json'}
 
 
-
-
-def main():
+def download_players(season):
     data = get_json()
     ids = get_ids(data)
     for i in ids:
-        path = f"data/20232024/players/{i}.json"
+        path = f"data/{season}/players/{i}.json"
         if os.path.exists(path):
             continue
         r = requests.get(f'https://api-web.nhle.com/v1/player/{i}/landing', headers=headers)
         print(f"Response: {r.json()}")
-        with open(f"data/20232024/players/{i}.json", 'w') as f:
+        with open(f"data/{season}/players/{i}.json", 'w') as f:
             json.dump(r.json(), f)
         time.sleep(2)
+
+def main():
+    # download_players('20232024')
+    pass
+
 
 
 if __name__ == '__main__':
