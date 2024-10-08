4fabd816a13a96b5979b46bc0f67fde9011c5816
Guigui031 <guillaumegenois@yahoo.ca>
2024-10-08 13:25:35 -0400

    add season_points and load

--- player.py
+++ player.py
@@ -3,6 +3,8 @@ import datetime
 
 class Player:
     def __init__(self, id):
+        self.seasons = {}
+        self.role = None
         self.salary_name = None
         self.position = None
         self.team = None
@@ -16,16 +18,12 @@ class Player:
     def set_name(self, first_name, last_name):
         self.name = first_name + ' ' + last_name
         self.salary_name = last_name + ', ' + first_name
-        
 
     def set_age(self, birth_date):
         self.birth_date = birth_date
         today = datetime.date.today()
         born = datetime.datetime.strptime(birth_date, '%Y-%m-%d')
         self.age = today.year - born.year - ((today.month, today.day) < (born.month, born.day))
-
-    def set_points(self, points):
-        self.points = points
 
     def set_salary(self, salary):
         self.salary = salary
@@ -35,9 +33,54 @@ class Player:
         
     def set_position(self, position_code):
         self.position = position_code
+        if position_code in ['C', 'L', 'R']:
+            self.role = 'A'
+        elif position_code == 'D':
+            self.role = 'D'
+        elif position_code == 'G':
+            self.role = 'G'
+
+    def set_season_points(self, season):
+        self.seasons[season] = Season(season)
+
+    def get_season_points(self, season):
+        return self.seasons[season].get_marqueur_points(self.role)
 
     def get_ratio_points_salary(self):
         return self.points / self.salary
     
     
+class Season:
+    def __init__(self, season_id):
+        self.n_games_played = None
+        self.n_assists = None
+        self.n_wins = None
+        self.n_shutouts = None
+        self.n_goals = None
+        self.season_id = season_id
 
+    def set_n_goals(self, n_goals):
+        self.n_goals = n_goals
+
+    def set_n_assists(self, n_assists):
+        self.n_assists = n_assists
+
+    def set_n_wins(self, n_wins):
+        self.n_wins = n_wins
+
+    def set_n_shutouts(self, n_shutouts):
+        self.n_shutouts = n_shutouts
+    
+    def set_n_games_played(self, n_games_played):
+        self.n_games_played = n_games_played
+
+    def get_marqueur_points(self, role):
+        if role == "A":
+            return 2 * self.n_goals + 1 * self.n_assists
+        elif role == "D":
+            return 3 * self.n_goals + 1 * self.n_assists
+        elif role == "G":
+            return 3 * self.n_wins + 5 * self.n_shutouts + 3 * self.n_goals + 1 * self.n_assists
+
+    def get_points(self):
+        return self.n_goals + self.n_assists
--- process_data.py
+++ process_data.py
@@ -27,8 +27,8 @@ def get_all_teams_abbrev(data):
     return abbrevs
 
 
-def get_skater_info_from_team(team, skater_id):
-    with open(f"data/20232024/teams/{team}.json") as f:
+def get_skater_info_from_team(season, team, skater_id):
+    with open(f"data/{season}/teams/{team}.json") as f:
         team_data = json.load(f)
     for player in team_data['skaters']:
         if str(player['playerId']) == skater_id:
@@ -44,6 +44,17 @@ def get_player_salary(name):
     return salary
 
 
+def load_season_points(pl_class):
+    for season in ['20202021', '20212022', '20222023', '20232024']:
+        pl_team_data = get_skater_info_from_team(season, pl_class.team, id)
+        pl_class.set_season_points(season)
+        pl_class.seasons[season].set_n_goals(pl_team_data['goals'])
+        pl_class.seasons[season].set_n_assists(pl_team_data['assists'])
+        pl_class.seasons[season].set_n_wins(pl_team_data['wins'])
+        pl_class.seasons[season].set_n_shutouts(pl_team_data['shutouts'])
+        pl_class.seasons[season].set_n_games_played(pl_team_data['gamesPlayed'])
+
+
 def load_player(id):
     pl_class = Player(id)
 
@@ -51,12 +62,12 @@ def load_player(id):
         pl_data = json.load(f)
 
     pl_class.set_team(pl_data['currentTeamAbbrev'])
-    pl_team_data = get_skater_info_from_team(pl_class.team, id)
     pl_class.set_age(pl_data['birthDate'])
     pl_class.set_name(pl_data['firstName']['default'], pl_data['lastName']['default'])
-    pl_class.set_points(pl_team_data['points'])
     pl_class.set_salary(get_player_salary(pl_class.salary_name))
     pl_class.set_position(pl_data['position'])
+    load_season_points(pl_class)
+
     return pl_class
 
 
